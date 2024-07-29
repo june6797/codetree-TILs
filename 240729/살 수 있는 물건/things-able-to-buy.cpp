9b0bde8943405a44620ea34a9db41c5042d57d97
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 14:46:25 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	double a;
+	cin >> a;
+	if (a >= 3000)
+	{
+		cout << "book";
+	}
+	else if (a >= 1000)
+	{
+		cout << "mask";
+	}
+	else
+	{
+		cout << "no";
+	}
+}
