297e7fcb829f4e37b30fa493dd00b9a2b808c79f
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 13:56:50 +0900

    Title: 길이 단위 환산하기, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cout << fixed << setprecision(1);
+	double a;
+	cin >> a;
+    cout << a * 30.48;
+}
