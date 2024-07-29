9fdf214addde19fcbd61739e0a85a65575d27590
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 18:36:42 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = n; i <= 5 * n; i+=n)
+	{
+		cout << i << " ";
+	}
+}
