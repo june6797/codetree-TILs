2f36709b4b01f25cd848d4c811e6d8bc29c78002
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 18:35:52 +0900

    Title: 증가시키며 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/증가시키며 출력하기/increase-and-print.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 5; i <= 17; i+=2)
+	{
+		cout << i << " ";
+	}
+}
