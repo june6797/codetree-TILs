1fd58b67c11f6ac8ab2048407333a9d9823b4b1f
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 18:24:06 +0900

    Title: 중앙값 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240729/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b, c;
+	cin >> a >> b >> c;
+	vector<int> v;
+	v.push_back(a);
+	v.push_back(b);
+	v.push_back(c);
+	sort(v.begin(), v.end());
+	cout >> v[1];
+}
