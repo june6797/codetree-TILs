49e3501745cc360e7f70e7676d56b41364f0fb18
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 14:28:02 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240729/합과 평균/sum-and-avg.cpp
+++ 240729/합과 평균/sum-and-avg.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
 using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
 
 int main() {
     cout << fixed << setprecision(1);
