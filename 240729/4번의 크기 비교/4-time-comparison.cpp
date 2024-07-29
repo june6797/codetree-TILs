55294d236750efaac45a9324714bbfd3b4937a6b
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 17:25:26 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240729/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	if (a >= b)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	cout << "\n";
+	if (a >= c)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	cout << "\n";
+	if (a >= d)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	cout << "\n";
+	if (a >=e)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	cout << "\n";
+}
