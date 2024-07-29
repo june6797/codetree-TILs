f45a180551c83030834ccca13c3b7525fc375556
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 17:24:53 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	if (a >= b)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	cout << "\n";
+	if (a > b)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	cout << "\n";
+	if (a <= b)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	cout << "\n";
+	if (a < b)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	cout << "\n";
+}
