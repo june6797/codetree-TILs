16dd275db9fcad9697e0451e9475509083f56d62
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 18:24:52 +0900

    Title: 차례로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/차례로 출력/print-in-order.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	for (int i = 5; i <= 17; i++)
+	{
+		cout << i << " ";
+	}
+}
