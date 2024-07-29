520be7aaa8bd7c86d1ec80ab6afa3e1ae886c8d0
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 14:56:04 +0900

    Title: 숫자의 조건 여부 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a;
+	cin >> a;
+	if (a == 5)
+	{
+		cout << "A";
+	}
+	if (a % 2 == 0)
+	{
+		cout << "B";
+	}
+}
