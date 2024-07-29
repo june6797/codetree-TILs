3330f9b0b65ea3efc57cb5af14f0b5fc3caa8324
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 14:21:04 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + 8 << "\n";
+    cout << b * 3 << "\n";
+    cout << (a + 8) * b * 3;
+    return 0;
+}
