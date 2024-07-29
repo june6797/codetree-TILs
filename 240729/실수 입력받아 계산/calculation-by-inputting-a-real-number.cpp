263491b67b2bd787abebe47943a4f95dfd4303db
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 14:00:36 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240729/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
+++ 240729/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
 using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
 
 int main() {
-    double a, b;
-    cin >> a>> b;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	double a, b;
+	cin >> a >> b;
 	cout << fixed << setprecision(2) << a + b;
-    return 0;
 }
