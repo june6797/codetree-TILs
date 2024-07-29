b566dff0ca308bfdc16dd7ec334a24073b93454a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 18:09:18 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	if (!a)
+	{
+		if (b >= 19)
+		{
+			cout << "MAN";
+		}
+		else
+		{
+			cout << "BOY";
+		}
+	}
+	else
+	{
+		if (b >= 19)
+		{
+			cout << "WOMAN";
+
+		}
+		else
+		{
+			cout << "GIRL";
+
+		}
+	}
+}
