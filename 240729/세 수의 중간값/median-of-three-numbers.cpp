5cca9f152b007ab9e7b4a3729cc28820c8e6e5ff
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-29 17:40:53 +0900

    Title: 세 수의 중간값, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b, c, d;
+	cin >> a >> b >> c;
+	if (b >= a && b < c)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+}
