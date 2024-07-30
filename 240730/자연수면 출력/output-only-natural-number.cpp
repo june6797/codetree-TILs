42e1cdc5dd314166e131f40156c59c84effe1be1
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-30 14:42:11 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	if (a > 0)
+	{
+		for (int i = 0; i < b; i++)
+		{
+			cout << a;
+		}
+	}
+	else
+	{
+		cout << 0;
+	}
+
+}
