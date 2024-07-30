ae971e09486f89aaad867e4a0b398e1d5343c87d
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-30 12:11:16 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240730/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	int temp = a / b;
+	cout << temp << ".";
+
+	int c = a;
+
+	int i = 0;
+
+	while (i <= 19)
+	{
+		c *= 10;
+		cout << c / b;
+		c = c % b;
+		i++;
+	}
+
+}
