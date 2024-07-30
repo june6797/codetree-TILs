686dde762707919e7d6499ae186251fbe5487fac
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-30 11:47:00 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b, n;
+	cin >> a >> b;
+	for (int i = a; i >= b; i -= 2)
+	{
+		cout << i << " ";
+	}
+}
