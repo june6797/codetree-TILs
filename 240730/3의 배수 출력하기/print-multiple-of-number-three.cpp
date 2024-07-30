9eb786f21afedcbe6dea5adcb31f32d14554fb2a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-30 11:44:21 +0900

    Title: 3의 배수 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/3의 배수 출력하기/print-multiple-of-number-three.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 3; i <= n; i+=3)
+	{
+		if (i % 3 == 0)
+		{
+			cout << i << " ";
+		}
+	}
+}
