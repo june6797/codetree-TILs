54f31f22de25a51a50ed272ee3a401dc97efd693
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-31 10:38:12 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++)
+	{
+		cout << i << " ";
+		if (i % 2 == 1)
+		{
+			i *= 2;
+			i--;
+		}
+		else
+		{
+			i += 2;
+		}
+	}
+}
