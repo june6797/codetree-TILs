91cac36b50ce6da796c016f6f5d6a06d066eda04
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-31 10:59:07 +0900

    Title: 윤년 횟수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 4 == 0)
+		{
+			if (i % 400 != 0 && i % 100 == 0)
+			{
+
+			}
+			ans++;
+		}
+	}
+	cout << ans;
+}
