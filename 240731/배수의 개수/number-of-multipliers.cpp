086a5661397a65afd71ed6e6e9b67c83f10bc47d
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-31 10:45:13 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	int ans = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		int num;
+		cin >> num;
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			ans++;
+		}
+	}
+	cout << ans;
+}
