f4e6aa56642214ac8b13e78c2ca2fc7496346141
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-31 11:07:51 +0900

    Title: 5의 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum;
+int a, b, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> a >> b;
+	for (int i = min(a, b); i <= max(a, b); i++)
+	{
+		if (i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	cout << sum;
+}
