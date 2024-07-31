86952d2a9cbe1a73ce7df6efacf91c9c4bd413c2
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-07-31 11:52:59 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/총합 구하기/find-the-total.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> a >> b;
+	for (int i = a; i <= b; i++)
+	{
+		if (i % 6 == 0 && i % 8 != 0)
+		{
+			sum += i;
+		}
+	}
+	cout << sum;
+}
