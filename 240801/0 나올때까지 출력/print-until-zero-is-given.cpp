a92780ac4de3784485ff6a3e055db8efeeb2725e
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-01 16:52:27 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	while (true)
+	{
+		int n; cin >> n;
+		if (n == 0)
+		{
+			break;
+		}
+		cout << n << "\n";
+	}
+}
