09e7f6f0fa15408237738d9dd424d7097fe41f0e
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-01 16:44:29 +0900

    Title: 1부터 곱셈하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+	ans = 1;
+	sum = 1;
+	while (sum < n)
+	{
+		sum *= ans;
+		ans++;
+	}
+	cout <<sum;
+}
