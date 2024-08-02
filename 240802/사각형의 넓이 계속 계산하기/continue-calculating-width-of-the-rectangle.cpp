6cff73b6d070c0a4b933546e05d19ffb87e66ca7
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-02 10:45:44 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	while (cnt < 3)
+	{
+		char c;
+		cin >> a >> b >>c ;
+		cout << a * b << "\n";
+		if (c =='C')
+		{
+			break;
+		}
+	}
+}
