ee2b754417320501570a629db63fdbe566bf446d
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-02 10:57:28 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, c, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> a >> b >> c;
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			cout << "C";
+			return 0;
+		}
+	}
+	cout << "P";
+
+}
