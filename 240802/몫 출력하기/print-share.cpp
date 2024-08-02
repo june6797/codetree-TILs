64b99ebf731c96a8375df622ff53fc5a936bbfb5
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-02 10:44:50 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/몫 출력하기/print-share.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	while (cnt < 3)
+	{
+		cin >> n;
+		if (n % 2 == 0)
+		{
+			cout << n / 2 << "\n";
+			cnt++;
+		}
+		
+	}
+}
