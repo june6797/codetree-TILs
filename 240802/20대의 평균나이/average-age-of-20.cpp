6216e77d50acbf82cd58ec18222a83af0ad863ae
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-02 10:47:12 +0900

    Title: 20대의 평균나이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/20대의 평균나이/average-age-of-20.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	while (true)
+	{
+		cin >> n;
+		if (n >= 30)
+		{
+			break;
+		}
+		cnt++;
+		sum += n;
+	}
+	cout << fixed << setprecision(2) << sum / cnt;
+}
