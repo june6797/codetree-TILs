70b12e2ac13e876121920d4f57cd80bbf7553485
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-02 11:00:25 +0900

    Title: 직사각형 별표 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/직사각형 별표 출력하기/print-stars-in-rectangle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, c, n, m;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+
+}
