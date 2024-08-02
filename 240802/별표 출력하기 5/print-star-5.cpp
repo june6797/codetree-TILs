7ecf7f4982f907f7d092bf726f6a729a566af343
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-02 11:06:19 +0900

    Title: 별표 출력하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, c, n, m;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = n - i; j > 0; j--)
+		{
+			for (int k = 0; k < n - i; k++)
+			{
+				cout << "*";
+			}
+			cout << " ";
+		}
+		cout << "\n";
+	}
+}
