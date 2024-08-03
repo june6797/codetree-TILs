2a5febce0bd74cdda7f9656a2b7c0850b3203f5f
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-03 12:28:27 +0900

    Title: 배열 채우고 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int ans, sum, cnt;
+int a, b, c, n, m;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string temp = "";
+	for (int i = 0; i < 10; i++)
+	{
+		char q;
+		cin >> q;
+		temp = q + temp;
+	}
+	cout << temp;
+}
