57053f2725bc61d100e3a0e70c9bc2fcb37d0f88
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 19:12:33 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+double ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a = 0, b = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> arr[i];
+		if (i / 2)
+		{
+			a += arr[i];
+		}
+		else
+		{
+			b += arr[i];
+		}
+	}
+	cout << abs(a - b);
+}
