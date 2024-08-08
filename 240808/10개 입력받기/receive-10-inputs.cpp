1b95f6ee8c7ab307f18b5b3d3e2834f1f66d3514
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 18:47:42 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int cnt = 0;
+	double sum = 0;
+	while (true)
+	{
+		int a;
+		cin >> a;
+		if (a == 0)
+		{
+			break;
+		}
+		cnt++;
+		sum += a;
+		arr[cnt] = a;
+	}
+	cout << sum << fixed << setprecision(1) << sum / cnt;
+}
