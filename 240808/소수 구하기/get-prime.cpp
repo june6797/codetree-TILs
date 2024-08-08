0e2f3aba049e880627c397e9e7bc6de0ed5b76fd
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 18:34:13 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/소수 구하기/get-prime.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+bool check(int n) {
+	int temp = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			temp ++;
+		}
+	}
+	if (temp == 2)
+	{
+		return true;
+	}
+	return false;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++)
+	{
+		if (check(i))
+		{
+			cout << i << " ";
+		}
+	}
+}
