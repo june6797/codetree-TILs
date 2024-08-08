4847404651cc348033eadb6923c16680df39efe9
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 19:10:37 +0900

    Title: 특정 위치의 문자 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+double ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> arr[i];
+	}
+	cout << (char)(arr[1]) << " " << (char)(arr[4]) << " " << (char)(arr[7]) << " ";
+}
