fe3cc90ddd62b36d378037d3cccda8aa052befc6
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 13:22:35 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+bool arr[16][16];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << n - j << " ";
+		}
+		cout << "\n";
+	}
+}
