b061b52bf437537ef7eab66fc41bcc9a94c3ec94
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 19:03:48 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/특정 위치의 숫자/numbers-in-specific-location.cpp
+++ 240808/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -13,10 +13,7 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-	stack<int> s;
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		cin >> arr[i];
 	}
