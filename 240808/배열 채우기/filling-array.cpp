c27958977ae36abdc3a2318d5a636c66f38bc87c
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 18:45:44 +0900

    Title: 배열 채우기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240808/배열 채우기/filling-array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int cnt = 0;
+	while (true)
+	{
+		int a;
+		cin >> a;
+		if (a == 0)
+		{
+			break;
+		}
+		cnt++;
+		arr[cnt] = a;
+	}
+	for (int i = 0; i < cnt; i++)
+	{
+		cout << arr[cnt - i] << " ";
+	}
+}
