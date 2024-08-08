263ae941e7a0ec69fcee26c3362af62cf427e5fe
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 16:16:20 +0900

    Title: 9-1 정사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/9-1 정사각형/9-1-square.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[101][101];
+int cnt;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	cnt = 10;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cnt--;
+			if (cnt == 0)
+			{
+				cnt =  9;
+			}
+			cout << cnt << " ";
+		}
+		cout << "\n";
+	}
+
+}
