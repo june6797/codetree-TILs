848b4876d317209d66f26f0f7d48c2ebbf32ff16
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 16:47:08 +0900

    Title: 특이한 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/특이한 출력/special-print.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[101][101];
+int cnt;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cout << "(" << i << ", " << j << ") ";
+			if (i + j % 4 == 0)
+			{
+				cout << "\n";
+			}
+		}
+	}
+}
