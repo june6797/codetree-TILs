6a88afeb6b5af22f43e142a336ad6a88829f6a8a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-08 17:02:58 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/3n + 1 수열 m번/3n-plus-1-sequence-m-times.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		int m;
+		cin >> m;
+		while (m != 1)
+		{
+			ans++;
+			if (m % 2)
+			{
+				m *= 3;
+				m++;
+			}
+			else
+			{
+				m /= 2;
+			}
+		}
+		cout << ans << "\n";
+	}
+}
