45ef2ae793e74fa2f1e3ce1953b7d794323360a7
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 21:57:49 +0900

    Title: 주사위 놀이, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/주사위 놀이/play-with-dice.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+double ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	for (int i = 0; i < 10; i++)
+	{
+		int n;
+		cin >> n;
+		arr[n]++;
+	}
+	for (int i = 1; i <= 9; i++)
+	{
+		cout << i << " - " << arr[i] << "\n";
+	}
+}
