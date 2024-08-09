fd6f0442784659af2c2a180b582442e5bbc4728f
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:51:47 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+string str[21];
+int ans, cnt;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str[i] << "\n";
+	}
+}
