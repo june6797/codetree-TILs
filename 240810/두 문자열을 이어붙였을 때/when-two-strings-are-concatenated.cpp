1116e64d3d4bac10261ae378af25ef1bd3a7eeca
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 01:06:23 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str, str2;
+	cin >> str >> str2;
+	if (str + str2 == str2 + str)
+	{
+		cout << "true";
+	}
+	else
+	{
+		cout << "false";
+	}
+}
