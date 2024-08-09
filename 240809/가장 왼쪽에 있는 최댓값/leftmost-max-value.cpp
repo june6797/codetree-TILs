6f303480832bd594af0a1d77e54dadcefb936f39
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 23:20:34 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+vector<int> v;
+int ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	int m = -1;
+	for (int i = 0; i < n; i++)
+	{
+		int num; cin >> num;
+		arr[i] = num;
+		m = max(m, num);
+	}
+	while (m != arr[0])
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (arr[i] == m)
+			{
+				cout << i << " ";
+				n = i;
+				break;
+			}
+		}
+
+		m = -1;
+		for (int i = 0; i < n; i++)
+		{
+			m = max(m, arr[i]);
+		}
+	}
+    cout << "1";
+}
