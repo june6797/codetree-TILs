5c1d12bbd84f42766269028eb0d1be767b021c5f
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:26:39 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/문자열의 길이 출력하기/print-string's-range.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int arr[1001][1001];
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	ans += str.size();
+	cin >> str;
+	ans += str.size();
+
+	cout << ans;
+}
