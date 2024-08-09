73280cde93f2f8fb74ed2dbb7c12c41b2d3ae7c6
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:42:35 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	for (int i = 0; i < 10; i++)
+	{
+		string str; cin >> str;
+		cout << str << "\n";
+	}
+}
