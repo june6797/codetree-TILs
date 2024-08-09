a2462fb164529b17c8475549e7352692ad5ef936
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:39:51 +0900

    Title: 문자열의 총 길이 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/문자열의 총 길이 구하기/find-the-length-of-the-string.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+string str[100];
+vector<int> v;
+int ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	getline(cin, str);
+	cout << str.size();
+}
