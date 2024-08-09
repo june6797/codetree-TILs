db2573bf6703a071869b57456601bf193a796d12
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:35:27 +0900

    Title: 문자 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/문자 개수 세기/count-char.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int arr[1001][1001];
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	getline(cin, str);
+	char a;
+	cin >> a;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == a)
+		{
+			ans++;
+		}
+	}
+	cout << ans;
+}
