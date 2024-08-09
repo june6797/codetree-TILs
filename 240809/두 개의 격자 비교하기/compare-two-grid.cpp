0d19aa74bd09abc471f71fc808bf70b772c8c9ee
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 23:34:56 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int arr[1001][1001];
+int arr2[1001][1001];
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> arr2[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (arr[i][j] == arr2[i][j])
+			{
+				cout << 1 << " ";
+			}
+			else
+			{
+				cout << 0 << " ";
+			}
+		}
+		cout << "\n";
+	}
+}
