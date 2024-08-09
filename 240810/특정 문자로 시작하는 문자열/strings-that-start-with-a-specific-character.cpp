84ec2d8497c10135ee249a825709c1ca82a44812
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:48:00 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+string str[10];
+int ans, cnt;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> str[i];
+	}
+	char a;
+	cin >> a;
+	bool flag = false;
+	for (int i = 0; i < 10; i++)
+	{
+		if (str[i][0] == a)
+		{
+			cnt++;
+			ans += str[i].size();
+		}
+	}
+	cout << cnt << " " << fixed << setprecision(2) << double(ans) / cnt;
+}
