f1aebc0f1e19f890579d34d2fd4b9abaa54dd9c2
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 01:19:55 +0900

    Title: 문자 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/문자 교체하기/changing-char.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	for (int i = 0; i < 2; i++)
+	{
+		cout << str[i];
+	}
+	cin >> str;
+	for (int i = 2; i < str.size(); i++)
+	{
+		cout << str[i];
+	}
+}
