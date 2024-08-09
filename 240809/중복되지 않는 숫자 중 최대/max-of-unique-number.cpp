55f63f3d6676a885bedf4e5cb42845f70990456c
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 23:15:37 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+int arr1[1001];
+vector<int> v;
+int ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int num; cin >> num;
+		arr[num]++;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i ] == 1)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+	cout << -1;
+}
