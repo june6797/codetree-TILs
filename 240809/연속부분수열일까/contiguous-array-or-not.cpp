653af1103886c5ffa1d1652b3b60b95feb9ffb93
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 22:37:26 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/연속부분수열일까/contiguous-array-or-not.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+int arr1[1001];
+int ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cin >> arr1[i];
+	}
+	bool f = false;
+	for (int i = 0; i < n - m; i++)
+	{
+		bool flag = false;
+		if (arr[i] == arr1[0])
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (arr[i + j] != arr1[j])
+				{
+					flag = true;
+					break;
+				}
+			}
+			if (!flag)
+			{
+				f = true;
+			}
+		}
+	}
+
+	if (f)
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		cout << "No";
+	}
+}
