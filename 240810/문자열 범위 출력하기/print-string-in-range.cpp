686a7192ec444981389605cc6a02745c8f80c15e
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:33:08 +0900

    Title: 문자열 범위 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int arr[1001][1001];
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	getline(cin, str);
+	for (int i = 2; i < 10; i++)
+	{
+		cout << str[i];
+	}
+}
