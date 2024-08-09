e621c3c1e7fcdc06508e657a486b51d874b82011
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 01:17:58 +0900

    Title: 부분 문자열의 개수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/부분 문자열의 개수/number-of-substrings.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	string t;
+	cin >> t;
+	for (int i = 0; i < str.size() - t.size() + 1; i++)
+	{
+		bool flag = false;
+		for (int j = 0; j < t.size(); j++)
+		{
+			if (str[i + j] != t[j])
+			{
+				flag = true;
+			}
+		}
+		if (!flag)
+		{
+			ans++;
+		}
+	}
+	cout << ans;
+}
