04a5a9a7cc935b58710d727a9ad28b198cec7b79
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:27:52 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int arr[1001][1001];
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str, str2;
+	cin >> str >> str2;
+	if (str.length() == str2.length())
+	{
+		cout << "same";
+	}
+	else if (str.length() > str2.length())
+	{
+		cout << str;
+	}
+	else
+	{
+		cout << str2;
+	}
+}
