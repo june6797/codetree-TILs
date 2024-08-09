626f26b20423921f09de23bfe17f28edcbdb4424
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 23:39:47 +0900

    Title: 지그재그로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240809/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int arr[1001][1001];
+int arr2[1001][1001];
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		if (i % 2 == 1)
+		{
+
+			for (int j = 0; j < n; j++)
+			{
+				arr[n - j - 1][i] = ans++;
+			}
+		}
+		else
+		{
+
+			for (int j = 0; j < n; j++)
+			{
+				arr[j][i] = ans++;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
