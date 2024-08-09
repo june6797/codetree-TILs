7c33f4c62a8a3f32290366fe68159723d8adc6bc
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 00:05:43 +0900

    Title: 격자에 점 그리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int arr[1001][1001];
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int a, b; cin >> a >> b;
+		arr[a - 1][b - 1] = ++ans;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (arr[i][j])
+			{
+				cout << 1 << " ";
+			}
+			else
+			{
+				cout << 0 << " ";
+			}
+		}
+		cout << "\n";
+	}
+}
