484afc8c162dc8c945561cb3cafcef8d4bd03670
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 23:27:49 +0900

    Title: 대문자로 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/대문자로 바꾸기/change-to-capital.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+char arr[1001][1001];
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << arr[i][j] - 'a' + 'A' << " ";
+		}
+		cout << "\n";
+	}
+}
