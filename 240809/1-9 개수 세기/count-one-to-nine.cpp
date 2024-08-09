0cfa0b5bfb0cf98d84f6b45ca17bcbf5665c2051
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 21:57:23 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240809/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+double ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int n;
+		cin >> n;
+		arr[n]++;
+	}
+	for (int i = 1; i <= 9; i++)
+	{
+		cout << arr[i] << "\n";
+	}
+}
