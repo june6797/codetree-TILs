badb6987440662ba40ae7c8f9d802f013f32fa8d
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 22:03:13 +0900

    Title: 코로나 메뉴얼2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/코로나 메뉴얼2/covid-manual2.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+double ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	for (int i = 0; i < 3; i++)
+	{
+		char a;
+		int n;
+		cin >> a >> n;
+		if (a == 'Y' && n >= 37)
+		{
+			arr[0]++;
+		}
+		else if (a == 'Y' && n < 37)
+		{
+			arr[1]++;
+		}
+		else if (n >= 37)
+		{
+			arr[2]++;
+		}
+		else
+		{
+			arr[3]++;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	if (arr[0] >= 2)
+	{
+		cout << "E";
+	}
+}
