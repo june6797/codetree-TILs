3b87f8d057344ee396d9c37453114f262d823f2e
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 22:08:33 +0900

    Title: 나눗셈의 나머지, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int arr[1001];
+double ans;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	int ans = 0;
+	while (a != 0)
+	{
+		arr[a % b]++;
+		a = a / b;
+	}
+	for (int i = 0; i < 1000; i++)
+	{
+		ans += (arr[i] * arr[i]);
+	}
+    cout << ans;
+}
