6a09783f017179699d633e3f6f778ff32bfff15e
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-09 23:32:53 +0900

    Title: 숫자 직사각형, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240809/숫자 직사각형/rectangle-of-numbers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans;
+int arr[1001][1001];
+
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << ++ans << " ";
+		}
+		cout << "\n";
+	}
+}
