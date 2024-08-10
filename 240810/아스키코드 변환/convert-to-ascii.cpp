f3ed134855ed30945f506980dc653b066066ce66
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 23:32:52 +0900

    Title: 아스키코드 변환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240810/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+string str, temp;
+stack<char> s;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	char a, int b;
+	cin >> a >> b;
+	cout << (int)a << " " << (char)b;
+}
