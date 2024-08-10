d406482f4cf44c87762323c1ea999718c476c70a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 23:27:56 +0900

    Title: 규칙에 따라 밀기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/규칙에 따라 밀기/push-by-the-rules.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+string str, temp;
+stack<char> s;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	string temp;
+	cin >> temp;
+	for (int i = 0; i < temp.size(); i++)
+	{
+		if (temp[i] == 'L')
+		{
+			str = str + str[0];
+			str.erase(str.begin());
+		}
+		else
+		{
+			str = str[str.size() - 1] + str;
+			str.erase(str.end() - 1);
+
+		}
+	}
+
+	cout << str << "\n";
+}
