6a932971c1f885f31f5c093aa35e68793b6ad75a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 10:49:19 +0900

    Title: 문자열 놀이, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/문자열 놀이/play-with-string.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	int q;
+	cin >> q;
+	for (int i = 0; i < q; i++)
+	{
+		int num; cin >> num;
+		if (num == 1)
+		{
+			int a, b;
+			cin >> a >> b;
+			char temp = str[a - 1];
+			for (int j = 0; j < str.size(); j++)
+			{
+				if (j == a-1)
+				{
+					cout << str[b - 1];
+					str[j] = str[b - 1];
+				}
+				else if (j == b - 1)
+				{
+					cout << str[a - 1];
+					str[j] = temp;
+				}
+				else
+				{
+					cout << str[j];
+				}
+			}
+			cout << "\n";
+		}
+		else
+		{
+			char a, b;
+			cin >> a >> b;
+			for (int j = 0; j < str.size(); j++)
+			{
+				if (str[j] == a)
+				{
+					cout << b;
+					str[j] = b;
+				}
+				else
+				{
+					cout << str[j];
+				}
+			}
+			cout << "\n";
+		}
+	}
+}
