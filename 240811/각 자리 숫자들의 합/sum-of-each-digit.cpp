1e50e0e6982fe60148c3a2da5ae4cc94e7dee4b2
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-11 00:09:45 +0900

    Title: 각 자리 숫자들의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240811/각 자리 숫자들의 합/sum-of-each-digit.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a;
+	string str = to_string(ca);
+	for (int i = 0; i < str.size(); i++)
+	{
+		ans += (str[i] - '0');
+	}
+	cout << ans;
+}
