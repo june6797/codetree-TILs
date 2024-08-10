93831eae43c500e190f7831fa0ba0d5e4ec4b2c9
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 12:43:24 +0900

    Title: 두 번째를 첫 번째로, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/두 번째를 첫 번째로/second-to-first.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == str[1])
+		{
+			cout << str[0];
+		}
+		else
+		{
+			cout << str[i];
+		}
+	}
+}
