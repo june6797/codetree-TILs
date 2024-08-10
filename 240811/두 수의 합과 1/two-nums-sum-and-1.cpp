1be2697dd607f7fb1d757a9af96b40daa7278eba
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-11 00:05:27 +0900

    Title: 두 수의 합과 1, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/두 수의 합과 1/two-nums-sum-and-1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	int c = a + b;
+	string str = to_string(c);
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '1')
+		{
+			cnt++;
+		}
+	}
+	cout << cnt;
+}
