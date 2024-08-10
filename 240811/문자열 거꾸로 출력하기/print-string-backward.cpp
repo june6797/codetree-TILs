c507cbb4f1da1243419071a08873183cb9a7d32e
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-11 00:14:24 +0900

    Title: 문자열 거꾸로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/문자열 거꾸로 출력하기/print-string-backward.cpp
+++ 240811/문자열 거꾸로 출력하기/print-string-backward.cpp
@@ -15,9 +15,7 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < 100; i++)
 	{
 		string temp; cin >> temp;
 		if (temp == "END")
