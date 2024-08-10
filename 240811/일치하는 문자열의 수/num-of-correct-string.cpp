e041e36e819aa6a0422b8655293ec227be2a0984
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-11 00:12:50 +0900

    Title: 일치하는 문자열의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/일치하는 문자열의 수/num-of-correct-string.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	string str;
+	cin >> str;
+	for (int i = 0; i < n; i++)
+	{
+		string temp; cin >> temp;
+		if (temp == str)
+		{
+			ans++;
+		}
+	}
+	cout << ans;
+}
