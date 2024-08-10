94449750d2e850f6ca4e09acbddb1a464ef4b6e5
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 19:06:00 +0900

    Title: e 제거하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/e 제거하기/e-to-remove.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+string str, temp;
+stack<char> s;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == 'e' && cnt == 0)
+		{
+			cnt++;
+		}
+		else
+		{
+			cout << str[i];
+		}
+	}
+}
