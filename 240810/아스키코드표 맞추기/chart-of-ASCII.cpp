921bfb3055e61063d32676677fccd663840a8a7d
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-10 23:31:37 +0900

    Title: 아스키코드표 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+string str, temp;
+stack<char> s;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	for (int i = 0; i < 5; i++)
+	{
+		int n; cin >> n;
+		cout << (char)n << " ";
+	}
+}
