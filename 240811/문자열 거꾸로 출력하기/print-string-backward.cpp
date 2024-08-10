82619877cb3b4a1a5a78673885130c3dd2ded319
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-11 00:14:06 +0900

    Title: 문자열 거꾸로 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240811/문자열 거꾸로 출력하기/print-string-backward.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+vector<int> v;
+int ans, cnt;
+int arr[26];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		string temp; cin >> temp;
+		if (temp == "END")
+		{
+			break;
+		}
+		reverse(temp.begin(), temp.end());
+		cout << temp << "\n";
+	}
+}
