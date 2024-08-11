562c97f0d31ba8d44fd94630e1ca8cf8461593d1
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-08-11 14:21:13 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240811/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int ans, cnt;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> 1;
+		}
+		cout << "\n";
+	}
+}
