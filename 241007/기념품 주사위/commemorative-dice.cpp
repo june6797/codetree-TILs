d19f8c5e56a05eda5d374ce7f5d1781ac1e18f27
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-10-07 19:07:04 +0900

    Title: 기념품 주사위, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241007/기념품 주사위/commemorative-dice.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define INF 1234567890
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+int a[6];
+int b[6];
+
+int gcd(int a, int b) {
+	if (b == 0)
+	{
+		return a;
+	}
+	else
+	{
+		return gcd(b, a % b);
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	for (int i = 0; i < 6; i++)
+	{
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		cin >> b[i];
+	}
+	int cnt = 0;
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			if (a[i] > b[i])
+			{
+				cnt++;
+			}
+		}
+	}
+	int k = gcd(cnt, 36);
+	cout << cnt / k << "/" << 36 / k;
+}
